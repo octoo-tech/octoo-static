1b7f60f718037230d1769f7adc7a1d98f3175d9f
Oleksiy Babich <oleksiy@octoo.us>
2023-12-30 12:01:33 -0800

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">OCTOO</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="mailto:ask@octoo.us"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Contact
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<svelte:fragment slot="footer">
+    <div class="space-y-10 text-center flex flex-col items-center">
+      <p class="code">&copy; OCTOO, 2023</p>
+    </div>
+	</svelte:fragment>
+	<slot />
+</AppShell>

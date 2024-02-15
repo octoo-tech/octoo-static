8cb87c552ceb560df277abb218034535260440b0
Oleksiy Babich <oleksiy@octoo.us>
2024-02-14 23:20:30 -0800

    TECH-74 apply white-ish custom theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin'
+import { octooTheme } from './octoo-theme'
 
 export default {
 	darkMode: 'class',
@@ -15,11 +16,8 @@ export default {
 		typography,
 		skeleton({
 			themes: {
-				preset: [
-					{
-						name: 'hamlindigo',
-						enhancements: true,
-					},
+				custom: [
+					octooTheme
 				],
 			},
 		}),
